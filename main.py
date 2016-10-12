df5c15dfe5a0e9f49b1edcb0ee75b727a9c68699
Yohei Koga <monotaro_EX@hotmail.com>
2016-10-12 13:40:38 +0900

    test2

--- main.py
+++ main.py
@@ -247,6 +247,7 @@ def predictor(data,batch,gpu = 0):
     return results
 
 def main():
+    ##test2_1
     logfile = open("gradient_cnn.log","a")
     date = datetime.now()
     startdate = date.strftime('%Y/%m/%d %H:%M:%S')
